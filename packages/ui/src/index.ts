135e951db6b7b27d2716839641e0843a9cc4b9d0
nibruad16 <nibruad16@gmail.com>
2026-05-06 00:19:52 +0300

    fix(ui): add barrel index and main export entry
    - Add src/index.ts re-exporting all 8 components from a single entry point
    - Add dot export in package.json so import from @repo/ui works
    - Add Tailwind @source directive in web globals.css to scan ui package

---
+++ packages/ui/src/index.ts
@@ -0,0 +1,8 @@
+export { Button } from "./button";
+export { Card, CardHeader, CardBody, CardFooter } from "./card";
+export { Badge } from "./badge";
+export { TaskCard } from "./taskCard";
+export { Modal } from "./modal";
+export { Input } from "./input";
+export { Select } from "./select";
+export { LoadingSpinner } from "./loadingSpinner";
